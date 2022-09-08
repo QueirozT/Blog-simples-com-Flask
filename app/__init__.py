508a86b504b052c14456aa11ea8f3b6af34dab71
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-09-08 18:12:06 -0300

    Setando o arquivo de configuração no construtor do flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from config import Config
 
 from .blog import bp_blog
 
 def create_app():
     app = Flask(__name__)
+    
+    app.config.from_object(Config)
 
     app.register_blueprint(bp_blog)
 
