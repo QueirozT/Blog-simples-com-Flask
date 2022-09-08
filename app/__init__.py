44aa2c96ba7429d34f0908abce34508fde2af273
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-09-08 19:17:32 -0300

    Configurando o babel no factor do flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_babel import Babel
 from config import Config
 
 from .blog import bp_blog
@@ -7,6 +8,8 @@ def create_app():
     app = Flask(__name__)
     
     app.config.from_object(Config)
+    
+    babel = Babel(app)
 
     app.register_blueprint(bp_blog)
 
