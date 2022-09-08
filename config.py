ed7a0763a4f34715e22f50c29efc6a9dcc42780e
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-09-08 18:11:09 -0300

    Criando um arquivo de configuração para o flask

---
+++ config.py
@@ -0,0 +1,14 @@
+from decouple import config
+
+
+class Config(object):
+    DEBUG = config(
+        'DEBUG',
+        cast=bool,
+        default=True
+    )
+    
+    SECRET_KEY = config(
+        'SECRET_KEY', 
+        default="você-nuna-vai-adivinhar"
+    )
