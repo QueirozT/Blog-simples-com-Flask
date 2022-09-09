e2dc1e2950cdeafeb2a048eb63d3427ff33f83a6
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-09-09 19:23:09 -0300

    Configurando as rotas para autenticar os usuários
    Estou importando o flask-login para fazer a autenticação dos usuário e o werkzeug para parsear as urls.
    
    Adicionei o decorador de login_required no index para levar o usuário a página de login, porque preciso que esteja logado para carregar o nome de usuário na pagina.
    
    Estou fazendo algumas verificações na rota de login, para verificar se o usuário está logado, e também estou verificando a url para fazer o redirecionamento correto caso o decorador de login_required seja usado.
    
    Na verificação de url da rota de login, estou usando o url_parser().netloc para evitar que a página seja redirecionada para links externos, aceitando apenas urls relativas (sem domínio definido).
    
    Também criei uma rota de logout para encerrar a sessão do usuário.

--- app/blog.py
+++ app/blog.py
@@ -1,8 +1,12 @@
 from flask import (
-    Blueprint, current_app, flash, jsonify, redirect, render_template, request, url_for
+    Blueprint, current_app, flash, jsonify, redirect, render_template, 
+    request, url_for
 )
+from flask_login import current_user, login_required, login_user, logout_user
+from werkzeug.urls import url_parse
 
 from app.forms import LoginForm
+from app.models import User
 
 
 bp_blog = Blueprint('blog', __name__)
@@ -10,8 +14,8 @@ bp_blog = Blueprint('blog', __name__)
 
 @bp_blog.route('/', methods=['GET'])
 @bp_blog.route('/index', methods=['GET'])
+@login_required
 def index():
-    user = {'username': 'Tiago'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -23,26 +27,34 @@ def index():
         }
     ]
 
-    return render_template(
-        'index.html', 
-        title='Página Inicial', 
-        user=user,
-        posts=posts
-    )
+    return render_template('index.html', title='Página Inicial', posts=posts)
 
 
 @bp_blog.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('blog.index'))
+
     form = LoginForm()
     if form.validate_on_submit():
-        flash(
-            f'Um login foi solicitado para {form.username.data}, \
-                lembrar={form.remember_me.data}'
-        )
-        return redirect(url_for('blog.index'))
+        user = User.query.filter_by(username=form.username.data).first()
+
+        if user is None or not user.check_password(form.password.data):
+            flash('Usuário ou senha inválidos')
+            return redirect(url_for('blog.login'))
+        
+        login_user(user, remember=form.remember_me.data)
+        
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('blog.index')
+
+        return redirect(next_page)
     
-    return render_template(
-        'login.html', 
-        title='Entrar', 
-        form=form
-    )
+    return render_template('login.html', title='Entrar', form=form)
+
+
+@bp_blog.route('/logout', methods=['GET'])
+def logout():
+    logout_user()
+    return redirect(url_for('blog.index'))
