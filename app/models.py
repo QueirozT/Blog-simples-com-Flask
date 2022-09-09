12e3be0883106b18d31a8bf23f385d4816fc06a5
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-09-09 19:15:04 -0300

    Importando o LoginManager e UserMixim do flask-login
    Como estou trabalhando com factory e módulos blueprint do flask, preciso importar o LoginManager e o UserMixim do flask-login dentro do models.
    
    Estou inicializando o LoginManager e as configurações do flask-login usando a instância do flask que está sendo carregada na função de configuração de models.
    
    estou definindo o load_user para que o flask-login possa fazer o gerenciamento de sessões coletando o usuário logado.
    
    Estou adicionando o UserMixim na herança do model User para não precisar adicionar manualmente os campos:
    is_authenticated, is_active, is_anonymous e get_id que o flask-login usa para funcionar corretamente.

--- app/models.py
+++ app/models.py
@@ -1,17 +1,30 @@
 from datetime import datetime
+from flask_login import LoginManager, UserMixin
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
 db = SQLAlchemy()
 
+login = LoginManager()
+
 
 def config(app):
     db.init_app(app)
     app.db = db
 
+    login.init_app(app)
+    login.login_view = 'blog.login'
+    login.login_message = 'Faça login para acessar esta página.'
+    login.login_message_category = 'info'
 
-class User(db.Model):
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
