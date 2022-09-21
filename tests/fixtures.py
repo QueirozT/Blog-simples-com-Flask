70e21d173ae7861ae35bf78bd8f3649df477dd72
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-09-21 20:55:16 -0300

    Criando fixtures para os testes
    Estou criando fixtures para usar nos testes.
    
    A fixture func_app() cria uma instância do flask dentro do escopo da função que for invocada, serve para testar coisas pontuais.
    
    A fixture modul_app() cria uma instância do flask dentro do escopo do módulo que for invocada, serve para testar implementações mais complexas.
    
    A fixture testing_client() também está no escopo do módulo, mas cria um cliente de teste, este cliente serve para testar rotas e  funcionalidades do flask.

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,21 +1,46 @@
-from config import Config
+from config import TestConfig
 from pytest import fixture
 
 from app import create_app
-
-
-class TestConfig(Config):
-    TESTING = True
-    DEBUG = True
-
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///'
+from app.models import db, User
     
 
-@fixture(scope='module')
-def app():
+@fixture(scope='function')
+def func_app():
     app = create_app(TestConfig)
 
     with app.app_context():
-        app.db.create_all()
+        db.create_all()
 
         yield app
+
+        db.drop_all()
+        db.session.remove()
+
+
+@fixture(scope='module')
+def modul_app():
+    app = create_app(TestConfig)
+
+    with app.app_context():
+        db.create_all()
+
+        yield app
+
+        db.drop_all()
+        db.session.remove()
+
+
+@fixture(scope='module')
+def testing_client():
+    app = create_app(TestConfig)
+
+    with app.test_client() as testing_client:
+        with app.app_context():
+            yield testing_client
+
+
+def create_user(name):
+    user = User(username=name, email=f'{name}@email.com')
+    user.set_password('senha')
+    return user
