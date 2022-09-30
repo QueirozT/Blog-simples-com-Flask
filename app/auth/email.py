89e1ac0e4967c1189bea09d367a654c5f6960d87
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-09-30 18:36:44 -0300

    [CORRIGIDO] Erro do link no email de recuperação
    Infelizmente uma letra fora do lugar estava causando o bug do link incompleto no email.

--- app/auth/email.py
+++ app/auth/email.py
@@ -13,6 +13,6 @@ def send_password_reset_email(user):
             'email/reset_password.txt', user=user, token=token
         ),
         html_body=render_template(
-            'email/reset_password.html', user=user, tokne=token
+            'email/reset_password.html', user=user, token=token
         )
     )
