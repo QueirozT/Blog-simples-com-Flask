3b9f4d1e9f7a5629aea35be585d35e3110b1e7cd
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-10-09 00:00:14 -0300

    Atualizando o contexto do flask
    Estou adicionando os novos modelos ao contexto do flask para facilitar o acesso nos testes e debug

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,13 @@
 from app import create_app
 
-from app.models import User, Post
+from app.models import User, Post, Notification, Message
 
 app = create_app()
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': app.db, 'User': User, 'Post': Post }
+    return {'db': app.db, 'User': User, 'Post': Post, 'Message': Message, 'Notification': Notification }
 
 
 if __name__ == '__main__':
