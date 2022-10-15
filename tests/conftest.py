76a9cb46d315a89c517f3407d3d412bd24b52ffd
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-10-14 23:11:31 -0300

    Criando o arquivo conftest e testes para as novas rotas
    Estou adicionando o arquivo conftest para que o pytest possa usar as fixtures de forma correta e aproveitei para criar alguns tests simples para validar as novas rotas.

---
+++ tests/conftest.py
@@ -0,0 +1,30 @@
+from config import TestConfig
+from pytest import fixture
+
+from app import create_app
+
+
+@fixture(scope='function')
+def app():
+    app = create_app(TestConfig)
+
+    with app.app_context():
+        app.db.create_all()
+
+        yield app
+
+        app.db.drop_all()
+        app.db.session.remove()
+
+
+@fixture(scope='module')
+def client():
+    app = create_app(TestConfig)
+    
+    app.test_request_context().push()
+    app.db.create_all()
+
+    yield app.test_client()
+
+    app.db.drop_all()
+    app.db.session.remove()
