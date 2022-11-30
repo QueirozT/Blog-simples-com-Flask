ee515c97938dfd8b1f8ac6f6bace0e5ea11bfbb4
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-11-30 18:00:19 -0300

    Configurando as extensões do projeto
    Estou adicionando duas extensões para trabalhar com markdown no projeto

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,13 @@
 from app import create_app
 
-from app.models import User, Post, Notification, Message
+from app.models import Notification, Post, Reply, User 
 
 app = create_app()
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': app.db, 'User': User, 'Post': Post, 'Message': Message, 'Notification': Notification }
+    return {'db': app.db, 'User': User, 'Post': Post, 'Reply': Reply, 'Notification': Notification }
 
 
 if __name__ == '__main__':
