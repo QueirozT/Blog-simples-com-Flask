3a8a83173188c6b294558b80bc5547fc74b2957e
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-11-30 17:59:22 -0300

    Atualizando os modelos do projeto
    Estou removendo as funcionalidades e a tabela de mensagens do projeto e adicionando a funcionalidade de resposta para os posts.

--- app/models.py
+++ app/models.py
@@ -41,7 +41,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
-    about_me = db.Column(db.String(200))
+    replies = db.relationship('Reply', backref='author', lazy='dynamic')
+    about_me = db.Column(db.Text)
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     followed = db.relationship(
         'User', 
@@ -49,18 +50,6 @@ class User(UserMixin, db.Model):
         primaryjoin=(followers.c.follower_id == id),
         secondaryjoin=(followers.c.followed_id == id),
         backref=db.backref('followers', lazy='dynamic'),
-        lazy='dynamic'
-    )
-    messages_sent = db.relationship(
-        'Message',
-        foreign_keys='Message.sender_id',
-        backref='author',
-        lazy='dynamic'
-    )
-    messages_received = db.relationship(
-        'Message',
-        foreign_keys='Message.recipient_id',
-        backref='recipient',
         lazy='dynamic'
     )
     last_message_read_time = db.Column(db.DateTime)
@@ -118,39 +107,29 @@ class User(UserMixin, db.Model):
         except:
             return
         return User.query.filter_by(id=id).first()
-    
-    def new_messages(self):
-        last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
-        return Message.query.filter_by(recipient=self).filter(
-            Message.timestamp > last_read_time
-        ).count()
 
-    def add_notification(self, name, data):
-        self.notifications.filter_by(name=name).delete()
-        n = Notification(name=name, payload_json=json.dumps(data), user=self)
-        db.session.add(n)
-        return n
+
+class Reply(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+
+    def __repr__(self):
+        return f'<Reply {self.body}>'
 
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(200))
+    title = db.Column(db.Text)
+    body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    replies = db.relationship('Reply', backref='answered', lazy='dynamic')
 
     def __repr__(self):
         return f'<Post {self.body}>'
-
-
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-
-    def __repr__(self):
-        return f'<Message {self.body}>'
 
 
 class Notification(db.Model):
